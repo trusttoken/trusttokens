9a57ea3789a314178284edbf67d5fed3d813c013
Ivan Rukhavets <ivanruch@gmail.com>
2020-05-11 15:11:02 +0300

    Implement registry() in TT mock

--- test/Factory.test.js
+++ test/Factory.test.js
@@ -29,7 +29,7 @@ contract('StakingOpportunityFactory', function(accounts) {
     beforeEach(async function() {
         this.registry = await Registry.new({ from: owner });
         this.rewardToken = await ValTokenWithHook.new({ from: issuer });
-        this.stakeToken = await TrustToken.new({ from: issuer });
+        this.stakeToken = await TrustToken.new(this.registry.address, { from: issuer });
         this.stakeToken.initialize({ from: issuer});
         await this.rewardToken.setRegistry(this.registry.address, {from: issuer})
         await this.rewardToken.mint(oneHundred, ONE_HUNDRED_ETHER, {from:issuer});
