fe0cd41d50499c0b727cbd849b95d5960a81aad9
hal <61718761+hal909@users.noreply.github.com>
2020-06-02 11:37:35 -0700

    lock pragma (#60)

--- contracts/StakedToken.sol
+++ contracts/StakedToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.13;
+pragma solidity 0.5.13;
 
 import { StakingAsset } from "./StakingAsset.sol";
 import { AStakedToken } from "./AStakedToken.sol";
--- contracts/StakingOpportunityFactory.sol
+++ contracts/StakingOpportunityFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.13;
+pragma solidity 0.5.13;
 
 import { StakedToken } from "./StakedToken.sol";
 import "./StakedTokenProxy.sol";
